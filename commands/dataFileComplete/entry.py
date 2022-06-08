c8f19da85cae6a331571511e3f7b71397c40f313
IronicMango <83050271+IronicMango@users.noreply.github.com>
2022-06-08 14:38:43 -0500

    Updating with offline changes

--- commands/dataFileComplete/entry.py
+++ commands/dataFileComplete/entry.py
@@ -1,11 +1,20 @@
+from asyncio.windows_events import NULL
 import csv
 import json
 import time
 
-import adsk.core
+import adsk.core, adsk.fusion
 
 from ... import config
 from ...lib import fusion360utils as futil
+
+import os
+import sys
+
+def remove_from_path(name):
+    if name in sys.path:
+        sys.path.remove(name)
+        remove_from_path(name)
 
 app = adsk.core.Application.get()
 ui = app.userInterface
@@ -57,7 +66,6 @@ def start():
     futil.log(f'**********local_handlers added: {len(local_handlers)}')
     futil.log(f'**********my_data_handlers added: {len(my_data_handlers)}')
 
-
 # Executed when add-in is stopped.  Remove events.
 def stop():
     futil.log(f'**********local_handlers stop: {len(local_handlers)}')
@@ -87,6 +95,20 @@ def handle_import(args: adsk.core.CustomEventArgs):
     # Keep track of imported files
     config.imported_documents[file_name] = new_document
     config.imported_filenames.append(file_name)
+
+    #Change the rope material
+    design = new_document.design
+    ##Get rope body
+    all_Components = design.allComponents
+    for component in all_Components:
+        body_Name = component.name
+        for name_ID in config.body_Name_IDs :
+            if name_ID in body_Name:
+                if component.bRepBodies.count != 0 :
+                    comp_Bodies = component.bRepBodies
+                    comp_Body = comp_Bodies.itemByName("Body1")
+                    comp_Body.appearance = config.custom_Appearance
+        
 
     # Fire event to save the document
     event_data = {
@@ -124,10 +146,6 @@ def handle_close(args: adsk.core.CustomEventArgs):
 def handle_data_file_complete(args: adsk.core.DataEventArgs):
     futil.log(f'***In application_data_file_complete event handler for: {args.file.name}')
 
-    # Get the dataFile and process it
-    # data_file: adsk.core.DataFile = args.file
-    # process_data_file(data_file)
-
     document: adsk.core.Document
     for file_name, document in config.imported_documents.items():
         if document.isValid:
@@ -144,12 +162,34 @@ def process_data_file(data_file: adsk.core.DataFile):
             public_link = data_file.publicLink
             futil.log(f"**********Created public link for {data_file.name}: {public_link}")
 
-            # Store the result of this file
-            config.results.append({
-                'Name': data_file.name,
-                'URN': data_file.versionId,
-                'Link': public_link
-            })
+            # Record Data
+            data = {
+                "fields": {
+                    "Name": data_file.name,
+                    "URN": data_file.versionId,
+                    "Link": public_link
+                }
+            }
+
+            # Write to Airtable
+            app_path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+            sys.path.insert(0, os.path.join(app_path, 'lib'))
+
+            # Post url and headers need to be here so that they pick up the changed API key
+            post_url = config.at_api_url
+            post_headers = {
+                'Authorization' : 'Bearer ' + config.at_key,
+                'Content-Type': 'application/json'
+            }
+
+            # The following line may register an error in VSCode. If your file structure is correct, this should
+            #   be inconsequential and will run correctly
+            import requests
+            post_airtable_request = requests.post(post_url, headers = post_headers, json = data)
+            print(post_airtable_request.status_code)
+            if post_airtable_request.status_code != 200 :
+                    config.export_Errors.append("Export error code: " + str(post_airtable_request.status_code) + "      File: " + data_file.name)
+            remove_from_path(os.path.join(app_path, 'lib'))
 
             config.imported_filenames.remove(data_file.name)
 
@@ -167,20 +207,17 @@ def process_data_file(data_file: adsk.core.DataFile):
         if len(config.imported_filenames) == 0:
             if not config.run_finished:
                 config.run_finished = True
-                write_results()
+            if not config.export_Errors :
+                ui.messageBox("Process Completed Succesfully. If files are left open, please use the 'Close All' function")
+            else :
+                error_Message = "The process completed with the following export errors: \n"
+                for e_Error in config.export_Errors :
+                    error_Message += str(e_Error)
+                    error_Message += "\n"
+                ui.messageBox(error_Message)
+                config.export_Errors =[]
+
     else:
         # futil.log(f"**********Already processed: {data_file.name}")
         ...
-
-
-# After all files are processed write the results
-def write_results():
-    futil.log(f"Writing CSV")
-    with open(config.csv_file_name, mode='w') as csv_file:
-        fieldnames = ['Name', 'URN', 'Link']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-        for row in config.results:
-            writer.writerow(row)
-
 
